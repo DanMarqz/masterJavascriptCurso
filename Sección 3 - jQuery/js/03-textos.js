ac3d6eea5a52333b75d98eac77f2facc78b343b3
Daniel <danielarturo.marquez@gmail.com>
2020-08-30 00:05:05 -0400

    terminado textos con jquery

--- Sección 3 - jQuery/js/03-textos.js
+++ Sección 3 - jQuery/js/03-textos.js
@@ -2,10 +2,23 @@ $(document).ready(function(){
   
   reloadLinks();
 
-  $('#add_btn').click(function(){
-    $('#menu').html('<li><a href="' + $('#add_link').val() + '"></a></li>');
-    reloadLinks();
-  });
+  $('#add_btn')
+// Eliminar atributos
+    .removeAttr('disabled')
+// Al clickar    
+    .click(function(){
+// Lo añade de primero
+      $('#menu').append('<li><a href="' + $('#add_link').val() + '"></a></li>');
+// Lo añade a lo último
+      //$('#menu').prepend('<li><a href="' + $('#add_link').val() + '"></a></li>');
+// Lo añade antes de la etiqueta ul
+      //$('#menu').before('<li><a href="' + $('#add_link').val() + '"></a></li>');
+// Lo añade después de la etiqueta ul
+      //$('#menu').after('<li><a href="' + $('#add_link').val() + '"></a></li>');
+
+      $('#add_link').val('');
+      reloadLinks();
+    });
 
 });
 
@@ -13,6 +26,9 @@ function reloadLinks(){
   $('a').each(function(index){
     var that = $(this);
     var enlace = that.attr('href');
+// Añade atributos adicionales    
+    that.attr('target','_blank');
+    
     that.text(enlace);
   });
 }
