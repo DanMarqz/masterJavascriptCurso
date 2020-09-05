9dcfb729f99cbe3293bd6d16ccdfcc87c7c1f0db
Daniel <danielarturo.marquez@gmail.com>
2020-09-04 22:31:20 -0400

    Sección lista,

--- Sección 3 - jQuery/js/06-interface.js
+++ Sección 3 - jQuery/js/06-interface.js
@@ -1,3 +1,46 @@
 $(document).ready(function(){
-  
+  //Draggable - Mover elemento por la página
+  $('.elemento').draggable();
+
+  //Resizable - Redimensionar los objetos
+  $('.elemento').resizable();
+
+// Sólo se puede usar uno a la vez o Selectable o Sortable
+
+  //Seleccionar elementos
+  //$('.lista-seleccionable').selectable();
+
+  //Sortable, organizar elementos
+  $('.lista-seleccionable').sortable({
+    update: function(event,ui){
+      console.log('Ha cambiado la lista');
+    }
+  });
+
+  //Drop
+  $('#elemento-movido').draggable();
+  $('#area').droppable({
+    drop: function(){
+      console.log('Has soltado algo dentro el área.')
+    }
+  });
+
+  //Efectos
+  $('#mostrar').click(function(){
+    $('.caja-efectos').toggle('shake',4000);
+  });
+
+  //Tooltip
+  $(document).tooltip();
+
+  //Dialog
+  $('#lanzar-popup').click(function(){
+    $('.popup').dialog();
+  });
+
+  //Datepicker
+  $('#calendario').datepicker();
+
+  //Tabs
+  $('#pestanas').tabs();
 })
